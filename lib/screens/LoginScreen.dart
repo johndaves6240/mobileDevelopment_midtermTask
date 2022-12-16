4d698904825de239ade6fae5151b8dd6e57c2ec1
johndaves6240 <johndaves6240@gmail.com>
2022-12-17 01:52:19 +0800

    loading icon

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -9,6 +9,7 @@ import 'package:midterm_task/widget/CustomTextField.dart';
 import 'package:midterm_task/widget/PasswordField.dart';
 import 'package:midterm_task/widget/PrimaryButton.dart';
 import 'package:midterm_task/widget/SecondaryButton.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class LoginScreen extends StatefulWidget {
   static String routeName = "/login";
@@ -24,66 +25,69 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   bool obscurePassword = true;
-
+  bool isLogginIn = false;
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Container(
         child: Center(
-          child: SingleChildScrollView(
-            child: Center(
-              child: Container(
-                width: width * .9,
-                child: Column(
-                  children: [
-                    CustomTextField(
-                        labelText: "Email Address",
-                        hintText: "Enter your email address",
-                        controller: emailController,
-                        textInputType: TextInputType.emailAddress),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    PasswordField(
-                        obscureText: obscurePassword,
-                        onTap: handleObscurePassword,
-                        labelText: "Password",
-                        hintText: "Enter your password",
-                        controller: passwordController),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    PrimaryButton(
-                        text: "Login",
+          child: ModalProgressHUD(
+            inAsyncCall: isLogginIn,
+            child: SingleChildScrollView(
+              child: Center(
+                child: Container(
+                  width: width * .9,
+                  child: Column(
+                    children: [
+                      CustomTextField(
+                          labelText: "Email Address",
+                          hintText: "Enter your email address",
+                          controller: emailController,
+                          textInputType: TextInputType.emailAddress),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      PasswordField(
+                          obscureText: obscurePassword,
+                          onTap: handleObscurePassword,
+                          labelText: "Password",
+                          hintText: "Enter your password",
+                          controller: passwordController),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      PrimaryButton(
+                          text: "Login",
+                          iconData: Icons.login,
+                          onPress: () {
+                            loginWithProvider();
+                          }),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      SecondaryButton(
+                        text: "Signup",
                         iconData: Icons.login,
                         onPress: () {
-                          loginWithProvider();
-                        }),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    SecondaryButton(
-                      text: "Signup",
-                      iconData: Icons.login,
-                      onPress: () {
-                        Navigator.pushNamed(context, SignupScreen.routeName);
-                      },
-                    ),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    Container(
-                        child: Center(
-                      child: GestureDetector(
-                        onTap: () {},
-                        child: const Text(
-                          "Don't have an account? Click Sign up button.",
-                          style: TextStyle(fontSize: 16.0),
-                        ),
+                          Navigator.pushNamed(context, SignupScreen.routeName);
+                        },
                       ),
-                    ))
-                  ],
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      Container(
+                          child: Center(
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: const Text(
+                            "Don't have an account? Click Sign up button.",
+                            style: TextStyle(fontSize: 16.0),
+                          ),
+                        ),
+                      ))
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -101,8 +105,16 @@ class _LoginScreenState extends State<LoginScreen> {
 
   loginWithProvider() async {
     try {
+      setState(() {
+        isLogginIn = true;
+      });
       var user = await _authService.signInWithGoogle();
+      // ignore: use_build_context_synchronously
       Navigator.pushReplacementNamed(context, DashboardScreen.routeName);
     } catch (e) {}
+
+    setState(() {
+      isLogginIn = false;
+    });
   }
 }
